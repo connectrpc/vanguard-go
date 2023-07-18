ade57a50968be1f5e85687afed537f2c4eea9e39
Edward McFarlane <emcfarlane@buf.build>
2023-07-19 00:41:37 +0100

    Add h2c support for hello

--- examples/envoy-plugin/hello.go
+++ examples/envoy-plugin/hello.go
@@ -10,14 +10,27 @@ import (
 	"time"
 
 	"golang.org/x/exp/slog"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
 func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(rsp http.ResponseWriter, req *http.Request) {
+		slog.Info("Request",
+			slog.String("proto", req.Proto),
+			slog.String("method", req.Method),
+			slog.String("url", req.URL.String()))
+		rsp.Header().Set("Hello-Header", "Hello, World!")
+		rsp.Header().Set("Trailer", "Hello-Trailer")
+		_, _ = rsp.Write([]byte("Hello, World!"))
+		rsp.Header().Set("Hello-Trailer", "Hello, World!")
+
+	})
 	svr := &http.Server{
-		Addr: ":8080",
-		Handler: http.HandlerFunc(func(rsp http.ResponseWriter, _ *http.Request) {
-			_, _ = rsp.Write([]byte("Hello, World!"))
-		}),
+		Addr:           ":8080",
+		Handler:        h2c.NewHandler(mux, &http2.Server{}),
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
