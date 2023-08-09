53edced1f218d036b71949e966e169a8b10b820c
Edward McFarlane <3036610+emcfarlane@users.noreply.github.com>
2023-08-09 19:39:59 +0100

    Parse parameters for variable captures (#9)
    * Parse parameters for variable captures
    
    * Use proto message for type generation
    
    * Move out unmarshalFieldMessage
    
    * Fix traverse spelling
    
    Co-authored-by: Joshua Humphries <2035234+jhump@users.noreply.github.com>
    
    * Fix unmarshal message types
    
    * Drop unsupported map type errors
    
    ---------
    
    Co-authored-by: Joshua Humphries <2035234+jhump@users.noreply.github.com>

---
+++ params.go
@@ -0,0 +1,187 @@
+// Copyright 2023 Buf Technologies, Inc.
+//
+// All rights reserved.
+
+package vanguard
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"connectrpc.com/connect"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+// setParameter sets the value of a field on a message using the ident fields.
+// Leaf fields must be a primitive type, or a well-known JSON scalar type.
+// Repeated fields of a primitive type are supported and will be appended to.
+// Map fields and other message types are not supported.
+//
+// See: https://github.com/googleapis/googleapis/blob/2c28ce13ade62398e152ff3eb840f4f934812597/google/api/http.proto#L117-L122
+func setParameter(msg protoreflect.Message, fields []protoreflect.FieldDescriptor, data []byte) error {
+	if len(fields) == 0 {
+		return connect.NewError(connect.CodeInternal,
+			fmt.Errorf("parameter missing field descriptors"),
+		)
+	}
+
+	// Traverse the message to the last field.
+	leaf, field := msg, fields[0]
+	for i := 0; i < len(fields)-1; i++ {
+		leaf, field = leaf.Mutable(field).Message(), fields[i+1]
+	}
+
+	value, err := unmarshalFieldValue(leaf, field, data)
+	if err != nil {
+		if jsonErr := (*json.UnmarshalTypeError)(nil); errors.As(err, &jsonErr) ||
+			// protojson errors are not exported, check the error string.
+			strings.HasPrefix(err.Error(), "proto") {
+			return connect.NewError(connect.CodeInvalidArgument,
+				fmt.Errorf("invalid parameter %q value for %s type: %s",
+					resolveFieldDescriptorsToPath(fields), field.Kind(), data,
+				),
+			)
+		}
+		return connect.NewError(connect.CodeInvalidArgument,
+			fmt.Errorf("invalid parameter %q %w",
+				resolveFieldDescriptorsToPath(fields), err,
+			),
+		)
+	}
+
+	// Set the value on the leaf message.
+	// Cannot be a map type, only lists, primitives or messages.
+	switch {
+	case field.IsList():
+		l := leaf.Mutable(field).List()
+		l.Append(value)
+	default:
+		leaf.Set(field, value)
+	}
+	return nil
+}
+
+func unmarshalFieldValue(msg protoreflect.Message, field protoreflect.FieldDescriptor, data []byte) (protoreflect.Value, error) {
+	//nolint:exhaustive
+	switch kind := field.Kind(); kind {
+	case protoreflect.BoolKind:
+		var b bool
+		if err := json.Unmarshal(data, &b); err != nil {
+			return protoreflect.Value{}, err
+		}
+		return protoreflect.ValueOfBool(b), nil
+	case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind:
+		var x int32
+		if err := json.Unmarshal(data, &x); err != nil {
+			return protoreflect.Value{}, err
+		}
+		return protoreflect.ValueOfInt32(x), nil
+	case protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind:
+		var x int64
+		if err := json.Unmarshal(data, &x); err != nil {
+			return protoreflect.Value{}, err
+		}
+		return protoreflect.ValueOfInt64(x), nil
+	case protoreflect.Uint32Kind, protoreflect.Fixed32Kind:
+		var x uint32
+		if err := json.Unmarshal(data, &x); err != nil {
+			return protoreflect.Value{}, err
+		}
+		return protoreflect.ValueOfUint32(x), nil
+	case protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
+		var x uint64
+		if err := json.Unmarshal(data, &x); err != nil {
+			return protoreflect.Value{}, err
+		}
+		return protoreflect.ValueOfUint64(x), nil
+	case protoreflect.FloatKind:
+		var x float32
+		if err := json.Unmarshal(data, &x); err != nil {
+			return protoreflect.Value{}, err
+		}
+		return protoreflect.ValueOfFloat32(x), nil
+	case protoreflect.DoubleKind:
+		var x float64
+		if err := json.Unmarshal(data, &x); err != nil {
+			return protoreflect.Value{}, err
+		}
+		return protoreflect.ValueOfFloat64(x), nil
+	case protoreflect.StringKind:
+		return protoreflect.ValueOfString(string(data)), nil
+	case protoreflect.BytesKind:
+		enc := base64.StdEncoding
+		if bytes.ContainsAny(data, "-_") {
+			enc = base64.URLEncoding
+		}
+		if len(data)%4 != 0 {
+			enc = enc.WithPadding(base64.NoPadding)
+		}
+		dst := make([]byte, enc.DecodedLen(len(data)))
+		n, err := enc.Decode(dst, data)
+		if err != nil {
+			return protoreflect.Value{}, err
+		}
+		return protoreflect.ValueOfBytes(dst[:n]), nil
+	case protoreflect.EnumKind:
+		var x protoreflect.EnumNumber
+		if err := json.Unmarshal(data, &x); err == nil {
+			return protoreflect.ValueOfEnum(x), nil
+		}
+		s := string(data)
+		if isNullValue(field) && s == "null" {
+			return protoreflect.ValueOfEnum(0), nil
+		}
+		enumVal := field.Enum().Values().ByName(protoreflect.Name(s))
+		if enumVal == nil {
+			return protoreflect.Value{}, fmt.Errorf("unknown enum: %s", data)
+		}
+		return protoreflect.ValueOf(enumVal.Number()), nil
+	case protoreflect.MessageKind:
+		return unmarshalFieldWKT(msg, field, data)
+	default:
+		return protoreflect.Value{}, fmt.Errorf("unsupported type %s", field.Kind())
+	}
+}
+
+// unmarshalFieldWKT unmarshals well known JSON scalars to their message types.
+func unmarshalFieldWKT(msg protoreflect.Message, field protoreflect.FieldDescriptor, data []byte) (protoreflect.Value, error) {
+	msgDesc := field.Message()
+	name := string(msgDesc.FullName())
+	if strings.HasPrefix(name, "google.protobuf.") {
+		switch name[16:] {
+		case "Timestamp", "Duration", "BytesValue", "StringValue", "FieldMask":
+			data = quote(data)
+			fallthrough
+		case "BoolValue", "Int32Value", "Int64Value", "UInt32Value",
+			"UInt64Value", "FloatValue", "DoubleValue":
+			return unmarshalFieldMessage(msg, field, data)
+		}
+	}
+	return protoreflect.Value{}, fmt.Errorf("unsupported message type %s", field.Message().FullName())
+}
+
+func unmarshalFieldMessage(msg protoreflect.Message, field protoreflect.FieldDescriptor, data []byte) (protoreflect.Value, error) {
+	value := msg.NewField(field)
+	if err := protojson.Unmarshal(data, value.Message().Interface()); err != nil {
+		return protoreflect.Value{}, err
+	}
+	return value, nil
+}
+
+func quote(raw []byte) []byte {
+	if len(raw) > 0 && (raw[0] != '"' || raw[len(raw)-1] != '"') {
+		raw = strconv.AppendQuote(raw[:0], string(raw))
+	}
+	return raw
+}
+
+func isNullValue(field protoreflect.FieldDescriptor) bool {
+	ed := field.Enum()
+	return ed != nil && ed.FullName() == "google.protobuf.NullValue"
+}
---
+++ params_test.go
@@ -0,0 +1,310 @@
+// Copyright 2023 Buf Technologies, Inc.
+//
+// All rights reserved.
+
+package vanguard
+
+import (
+	"encoding/base64"
+	"math"
+	"strconv"
+	"testing"
+	"time"
+
+	testv1 "github.com/bufbuild/vanguard/internal/gen/buf/vanguard/test/v1"
+	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/testing/protocmp"
+	"google.golang.org/protobuf/types/dynamicpb"
+	"google.golang.org/protobuf/types/known/durationpb"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+)
+
+func TestSetParameter(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		fields  string
+		value   string
+		initial proto.Message // initial message state, can be nil.
+		want    proto.Message // expected message state, must not be nil.
+		wantErr string
+	}{{
+		fields: "double_value",
+		value:  strconv.FormatFloat(math.MaxFloat64, 'f', -1, 64),
+		want:   &testv1.ParameterValues{DoubleValue: math.MaxFloat64},
+	}, {
+		fields: "float_value",
+		value:  strconv.FormatFloat(math.MaxFloat32, 'f', -1, 32),
+		want:   &testv1.ParameterValues{FloatValue: math.MaxFloat32},
+	}, {
+		fields: "int32_value",
+		value:  strconv.FormatInt(math.MaxInt32, 10),
+		want:   &testv1.ParameterValues{Int32Value: math.MaxInt32},
+	}, {
+		fields:  "int32_value",
+		value:   strconv.FormatInt(math.MaxInt64, 10),
+		want:    &testv1.ParameterValues{},
+		wantErr: "invalid_argument: invalid parameter \"int32_value\" value for int32 type: 9223372036854775807",
+	}, {
+		fields: "int64_value",
+		value:  strconv.FormatInt(math.MaxInt64, 10),
+		want:   &testv1.ParameterValues{Int64Value: math.MaxInt64},
+	}, {
+		fields: "uint32_value",
+		value:  strconv.FormatUint(math.MaxUint32, 10),
+		want:   &testv1.ParameterValues{Uint32Value: math.MaxUint32},
+	}, {
+		fields: "uint64_value",
+		value:  strconv.FormatUint(math.MaxUint64, 10),
+		want:   &testv1.ParameterValues{Uint64Value: math.MaxUint64},
+	}, {
+		fields: "sint32_value",
+		value:  strconv.FormatUint(math.MaxInt32, 10),
+		want:   &testv1.ParameterValues{Sint32Value: math.MaxInt32},
+	}, {
+		fields: "sint64_value",
+		value:  strconv.FormatUint(math.MaxInt64, 10),
+		want:   &testv1.ParameterValues{Sint64Value: math.MaxInt64},
+	}, {
+		fields: "bool_value",
+		value:  "true",
+		want:   &testv1.ParameterValues{BoolValue: true},
+	}, {
+		fields: "bool_value",
+		value:  "false",
+		want:   &testv1.ParameterValues{BoolValue: false},
+	}, {
+		fields: "string_value",
+		value:  "hello world",
+		want:   &testv1.ParameterValues{StringValue: "hello world"},
+	}, {
+		fields: "bytes_value",
+		value:  base64.StdEncoding.EncodeToString([]byte("abc123!?$*&()'-=@~")),
+		want:   &testv1.ParameterValues{BytesValue: []byte("abc123!?$*&()'-=@~")},
+	}, {
+		fields: "bytes_value",
+		value:  base64.URLEncoding.EncodeToString([]byte("abc123!?$*&()'-=@~")),
+		want:   &testv1.ParameterValues{BytesValue: []byte("abc123!?$*&()'-=@~")},
+	}, {
+		fields: "bytes_value",
+		value:  base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString([]byte("abc123!?$*&()'-=@")),
+		want:   &testv1.ParameterValues{BytesValue: []byte("abc123!?$*&()'-=@")},
+	}, {
+		fields: "timestamp",
+		value:  "2021-01-01T00:00:00Z",
+		want: &testv1.ParameterValues{
+			Timestamp: timestamppb.New(time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)),
+		},
+	}, {
+		fields: "duration",
+		value:  "1s",
+		want: &testv1.ParameterValues{
+			Duration: &durationpb.Duration{Seconds: 1},
+		},
+	}, {
+		fields: "duration",
+		value:  "3.000000001s",
+		want: &testv1.ParameterValues{
+			Duration: &durationpb.Duration{Seconds: 3, Nanos: 1},
+		},
+	}, {
+		fields: "bool_value_wrapper",
+		value:  "true",
+		want: &testv1.ParameterValues{
+			BoolValueWrapper: &wrapperspb.BoolValue{Value: true},
+		},
+	}, {
+		fields: "int32_value_wrapper",
+		value:  strconv.FormatInt(math.MaxInt32, 10),
+		want: &testv1.ParameterValues{
+			Int32ValueWrapper: &wrapperspb.Int32Value{Value: math.MaxInt32},
+		},
+	}, {
+		fields:  "int32_value_wrapper",
+		value:   strconv.FormatInt(math.MaxInt64, 10),
+		want:    &testv1.ParameterValues{},
+		wantErr: "invalid_argument: invalid parameter \"int32_value_wrapper\" value for message type: 9223372036854775807",
+	}, {
+		fields: "int64_value_wrapper",
+		value:  strconv.FormatInt(math.MaxInt64, 10),
+		want: &testv1.ParameterValues{
+			Int64ValueWrapper: &wrapperspb.Int64Value{Value: math.MaxInt64},
+		},
+	}, {
+		fields: "uint32_value_wrapper",
+		value:  strconv.FormatUint(math.MaxUint32, 10),
+		want: &testv1.ParameterValues{
+			Uint32ValueWrapper: &wrapperspb.UInt32Value{Value: math.MaxUint32},
+		},
+	}, {
+		fields: "uint64_value_wrapper",
+		value:  strconv.FormatUint(math.MaxUint64, 10),
+		want: &testv1.ParameterValues{
+			Uint64ValueWrapper: &wrapperspb.UInt64Value{Value: math.MaxUint64},
+		},
+	}, {
+		fields: "double_value_wrapper",
+		value:  strconv.FormatFloat(math.MaxFloat64, 'f', -1, 64),
+		want: &testv1.ParameterValues{
+			DoubleValueWrapper: &wrapperspb.DoubleValue{Value: math.MaxFloat64},
+		},
+	}, {
+		fields: "float_value_wrapper",
+		value:  strconv.FormatFloat(math.MaxFloat32, 'f', -1, 32),
+		want: &testv1.ParameterValues{
+			FloatValueWrapper: &wrapperspb.FloatValue{Value: math.MaxFloat32},
+		},
+	}, {
+		fields: "bytes_value_wrapper",
+		value:  base64.StdEncoding.EncodeToString([]byte("abc123!?$*&()'-=@~")),
+		want: &testv1.ParameterValues{
+			BytesValueWrapper: &wrapperspb.BytesValue{Value: []byte("abc123!?$*&()'-=@~")},
+		},
+	}, {
+		fields: "bytes_value_wrapper",
+		value:  base64.URLEncoding.EncodeToString([]byte("abc123!?$*&()'-=@~")),
+		want: &testv1.ParameterValues{
+			BytesValueWrapper: &wrapperspb.BytesValue{Value: []byte("abc123!?$*&()'-=@~")},
+		},
+	}, {
+		fields: "bytes_value_wrapper",
+		value:  base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString([]byte("abc123!?$*&()'-=@")),
+		want: &testv1.ParameterValues{
+			BytesValueWrapper: &wrapperspb.BytesValue{Value: []byte("abc123!?$*&()'-=@")},
+		},
+	}, {
+		fields: "string_value_wrapper",
+		value:  "hello world",
+		want: &testv1.ParameterValues{
+			StringValueWrapper: &wrapperspb.StringValue{Value: "hello world"},
+		},
+	}, {
+		fields: "string_value_wrapper",
+		value:  "\"hello world\"",
+		want: &testv1.ParameterValues{
+			StringValueWrapper: &wrapperspb.StringValue{Value: "hello world"},
+		},
+	}, {
+		fields: "field_mask",
+		value:  "foo,bar.baz", // TODO: validate against the descriptor?
+		want: &testv1.ParameterValues{
+			FieldMask: &fieldmaskpb.FieldMask{Paths: []string{"foo", "bar.baz"}},
+		},
+	}, {
+		fields: "enum_value",
+		value:  "1",
+		want: &testv1.ParameterValues{
+			EnumValue: testv1.ParameterValues_ENUM_VALUE,
+		},
+	}, {
+		fields: "enum_value",
+		value:  "ENUM_VALUE",
+		want: &testv1.ParameterValues{
+			EnumValue: testv1.ParameterValues_ENUM_VALUE,
+		},
+	}, {
+		fields: "enum_value",
+		value:  "null",
+		want: &testv1.ParameterValues{
+			EnumValue: testv1.ParameterValues_ENUM_UNSPECIFIED,
+		},
+	}, {
+		fields:  "enum_value",
+		value:   "unknown",
+		want:    &testv1.ParameterValues{},
+		wantErr: "invalid_argument: invalid parameter \"enum_value\" unknown enum: unknown",
+	}, {
+		fields: "enum_list",
+		value:  "1",
+		initial: &testv1.ParameterValues{
+			EnumList: []testv1.ParameterValues_Enum{
+				testv1.ParameterValues_ENUM_UNSPECIFIED,
+			},
+		},
+		want: &testv1.ParameterValues{
+			EnumList: []testv1.ParameterValues_Enum{
+				testv1.ParameterValues_ENUM_UNSPECIFIED,
+				testv1.ParameterValues_ENUM_VALUE,
+			},
+		},
+	}, {
+		fields: "oneof_double_value",
+		value:  "1.234",
+		want: &testv1.ParameterValues{
+			Oneof: &testv1.ParameterValues_OneofDoubleValue{
+				OneofDoubleValue: 1.234,
+			},
+		},
+	}, {
+		fields: "nested.double_value",
+		value:  "1.234",
+		want: &testv1.ParameterValues{
+			Nested: &testv1.ParameterValues_Nested{
+				DoubleValue: 1.234,
+			},
+		},
+	}, {
+		fields: "recursive.double_value",
+		value:  "1.234",
+		want: &testv1.ParameterValues{
+			Recursive: &testv1.ParameterValues{
+				DoubleValue: 1.234,
+			},
+		},
+	}, {
+		fields:  "string_map",
+		value:   "hello",
+		want:    &testv1.ParameterValues{},
+		wantErr: "invalid_argument: invalid parameter \"string_map\" unsupported message type buf.vanguard.test.v1.ParameterValues.StringMapEntry",
+	}, {
+		fields:  "nested_map.double_value",
+		value:   "1.234",
+		want:    &testv1.ParameterValues{},
+		wantErr: "in field path \"nested_map.double_value\": field \"nested_map\" of type buf.vanguard.test.v1.ParameterValues should not be a list or map",
+	}, {
+		fields: "timestamp",
+		value:  "2021-01-01T00:00:00Z",
+		want: func() proto.Message {
+			msg := dynamicpb.NewMessage((&testv1.ParameterValues{}).ProtoReflect().Descriptor())
+			field := msg.Descriptor().Fields().ByName("timestamp")
+			input := &timestamppb.Timestamp{Seconds: 1609459200}
+			value := protoreflect.ValueOfMessage(input.ProtoReflect())
+			msg.Set(field, value)
+			return msg
+		}(),
+	}}
+	for _, testCase := range testCases {
+		testCase := testCase
+		t.Run(testCase.fields+"="+testCase.value, func(t *testing.T) {
+			t.Parallel()
+
+			desc := testCase.want.ProtoReflect().Descriptor()
+			fields, err := resolvePathToDescriptors(desc, testCase.fields)
+			if err != nil {
+				assert.Equal(t, testCase.wantErr, err.Error())
+				return
+			}
+			require.NoError(t, err)
+
+			value := testCase.want.ProtoReflect().New()
+			if testCase.initial != nil {
+				proto.Merge(value.Interface(), testCase.initial)
+			}
+
+			if err := setParameter(value, fields, []byte(testCase.value)); err != nil {
+				assert.Equal(t, testCase.wantErr, err.Error())
+				return
+			}
+			require.NoError(t, err)
+
+			got := value.Interface()
+			assert.Empty(t, cmp.Diff(testCase.want, got, protocmp.Transform()))
+		})
+	}
+}
